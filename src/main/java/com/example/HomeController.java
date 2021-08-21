2f8ea5f0b9967c8f7db1840d88c3be835375ce4f
Perepelytsia <perepelytsa@devoler.com>
2021-08-21 23:58:08 +0300

    fix project

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.google.common.base.Charsets;
-import com.google.common.io.CharSink;
-import com.google.common.io.FileWriteMode;
-import com.google.common.io.Files;
-import java.io.File;
-import java.io.IOException;
 
 @RestController
 public class HomeController {
@@ -26,16 +20,6 @@ public class HomeController {
     public Map<String, String> getPost(@RequestBody String payload) {
         
         System.out.println(payload);
-
-        try {
-           String fileName = "src/main/resources/request.log";
-        File file = new File(fileName);
-        CharSink chs = Files.asCharSink(file, Charsets.UTF_8, FileWriteMode.APPEND);
-        chs.write(payload+"\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
         
         Map<String, String> map = new HashMap<String, String>();
         map.put("result", "success");

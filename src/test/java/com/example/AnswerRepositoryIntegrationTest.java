08974a1f006bc3b0828935f59e3b6a2c5840eaf9
Perepelytsia <perepelytsa@devoler.com>
2021-09-10 11:11:15 +0300

    refactor tasks

--- src/test/java/com/example/AnswerRepositoryIntegrationTest.java
+++ src/test/java/com/example/AnswerRepositoryIntegrationTest.java
@@ -7,9 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class AnswerRepositoryIntegrationTest {
     @Autowired
     protected AnswerRepository repoAnswer;

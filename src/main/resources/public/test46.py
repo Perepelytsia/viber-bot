169e35f1ba96fd99e22a9977b9dcd9c9d48d77bc
Perepelytsia <perepelytsa@devoler.com>
2021-09-28 21:30:28 +0300

    change tasks

---
+++ src/main/resources/public/test46.py
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------------
+# https://www.youtube.com/watch?v=ypoE3bD2xbI&ab_channel=%D0%93%D0%BE%D1%88%D0%B0%D0%94%D1%83%D0%B4%D0%B0%D1%80%D1%8C
+# https://www.youtube.com/watch?v=oRr_bEXJbV0&ab_channel=egoroff_channel
+# https://www.youtube.com/watch?v=7-rVPqE5YOk&ab_channel=%D0%98%D0%B7%D1%83%D1%87%D0%B0%D0%B5%D0%BC%D0%BC%D0%B8%D1%80%D0%98%D0%A2%2F%D0%9E%D0%BB%D0%B5%D0%B3%D0%A8%D0%BF%D0%B0%D0%B3%D0%B8%D0%BD%2FWISEPLAT
+# https://www.youtube.com/watch?v=3lpqHCvHOAk&ab_channel=%D0%98%D0%B7%D1%83%D1%87%D0%B0%D0%B5%D0%BC%D0%BC%D0%B8%D1%80%D0%98%D0%A2%2F%D0%9E%D0%BB%D0%B5%D0%B3%D0%A8%D0%BF%D0%B0%D0%B3%D0%B8%D0%BD%2FWISEPLAT
+# https://www.youtube.com/watch?v=zNvNGTUN-ck&ab_channel=pythonsamurai
+# ------------------------------------------------------------------------

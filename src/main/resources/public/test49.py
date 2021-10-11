d4d44ddd2a2c49d5c91cadd6c3082615d6b3391f
Perepelytsia <perepelytsa@devoler.com>
2021-10-11 13:59:58 +0300

    add tasks

---
+++ src/main/resources/public/test49.py
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------------
+# 1) https://pythonworld.ru/osnovy/rabota-s-modulyami-sozdanie-podklyuchenie-instrukciyami-import-i-from.html (Создание своего модуля на Python)
+# 2) https://abc-server.com/ru/blog/administration/create-python-3-modules/
+# 3) https://pythobyte.com/creating-a-python-module-2b202310/
+# 4) переделать задание test47.py и test48.py - вынести опредедение функций и классов в отдельные модули.
+# 5) и импортировать их в те файлы в которых происходит их вызов (запуск программы)
+# ------------------------------------------------------------------------

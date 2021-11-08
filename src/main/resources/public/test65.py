25343705cd8d798dc6220b6fa656091599b05849
Perepelytsia <perepelytsa@devoler.com>
2021-11-08 16:35:50 +0200

    add git tasks

--- src/main/resources/public/test65.py
+++ src/main/resources/public/test65.py
@@ -1,4 +1,4 @@
-1) Скачать проект https://github.com/Cledersonbc/tic-tac-toe-minimax
+1) git clone https://github.com/Cledersonbc/tic-tac-toe-minimax
 2) Запустить в консоли и проверить работоспособность
 3) Поменять логику - если поставил 2 то человек может выиграть, для бота оставить стандартную логику
 4) Проверить логику
